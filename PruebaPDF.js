d77e6b33e30f25b152d096056c33dfee99172adb
AxelLaraG <axmadlar@gmail.com>
2024-07-09 12:08:51 -0600

    first commit

---
+++ PruebaPDF.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+function encontrar_pdfs(ruta) {
+    const pdfs = [];
+    const archivos = fs.readdirSync(ruta);
+    for (const archivo of archivos) {
+        const rutaCompleta = path.join(ruta, archivo);
+        const stats = fs.statSync(rutaCompleta);
+        if (stats.isFile() && archivo.endsWith(".pdf")) {
+            pdfs.push(rutaCompleta);
+        } else if (stats.isDirectory()) {
+            pdfs.push(...encontrar_pdfs(rutaCompleta));
+        }
+    }
+    return pdfs;
+}
+
+const ruta_entrada = "Path";
+const pdfs_encontrados = encontrar_pdfs(ruta_entrada);
+console.log(pdfs_encontrados);
+console.log(pdfs_encontrados[1]);
